26c76d92626ea4ab08b0a2a5d8fa4522eefbafa0
二叉树树 <acofork@qq.com>
2026-01-17 18:48:23 +0800

    Add short link: ScVEG4X

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2198,5 +2198,9 @@ window.RULES_INTERMEDIATE = {
     "/WrACkNE": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/ScVEG4X": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
