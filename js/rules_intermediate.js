8aa6f1fbabebba82a785333a6495f761c945dcdf
二叉树树 <acofork@qq.com>
2026-01-17 19:01:05 +0800

    Add short link: KBkJR8n

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2754,5 +2754,9 @@ window.RULES_INTERMEDIATE = {
     "/CMtxYRS": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/KBkJR8n": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
