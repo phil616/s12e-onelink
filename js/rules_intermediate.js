b0f161f903f67611c914611d024dc9b38a45f521
二叉树树 <acofork@qq.com>
2026-01-17 18:50:28 +0800

    Add short link: KmVIUkl

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2374,5 +2374,9 @@ window.RULES_INTERMEDIATE = {
     "/P1DpLAr": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/KmVIUkl": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
