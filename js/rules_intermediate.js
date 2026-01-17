eacfe3560289741e29d1c5cae8d4c7e670bfe72a
二叉树树 <acofork@qq.com>
2026-01-17 19:00:42 +0800

    Add short link: t2HQenf

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2690,5 +2690,9 @@ window.RULES_INTERMEDIATE = {
     "/86MXh7a": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/t2HQenf": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
