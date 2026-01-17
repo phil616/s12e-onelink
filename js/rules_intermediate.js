6cf9a4d2ff4338db2fb674e030cd1eca50532293
二叉树树 <acofork@qq.com>
2026-01-17 18:49:32 +0800

    Add short link: l6zBTeu

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2294,5 +2294,9 @@ window.RULES_INTERMEDIATE = {
     "/M2SRkpx": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/l6zBTeu": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
